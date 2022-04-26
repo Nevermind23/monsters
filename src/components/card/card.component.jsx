f8bd118882c5e00ae881124be7b03c706b1b0900
Nevermind23 <tyeshela7@gmail.com>
2022-04-26 13:37:40 +0400

    Set fixed image size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,8 +4,10 @@ import "./card.style.css";
 class CardComponent extends Component {
     render() {
         return (<div className="card-container">
-            <img src={`https://robohash.org/${this.props.monster.id}?set=set2&size=180x180`}
-                 alt={`Monster ${this.props.monster.name}`} />
+            <div className='card-image'>
+                <img src={`https://robohash.org/${this.props.monster.id}?set=set2&size=250x250`}
+                     alt={`Monster ${this.props.monster.name}`} width='250px' height='250px' />
+            </div>
             <div className="card-title">{this.props.monster.name}</div>
         </div>);
     }
